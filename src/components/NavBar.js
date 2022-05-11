bc9ee60536823d371135e5f6bb01b384a3df744a
marcalph <alphonsus.marc@gmail.com>
2022-05-11 07:49:44 +0200

    mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { MdClose, MdMenu } from 'react-icons/md';
 
 const NavBarStyles = styled.div`
   position: fixed;
@@ -9,50 +10,150 @@ const NavBarStyles = styled.div`
   width: 100%;
   padding: 1rem 0;
   background: var(--dark-bg);
-  ul{
+  ul {
     max-width: 1200px;
     margin: 0 auto;
-    width; 90%;
+    width: 90%;
     text-align: center;
-    li{
+    li {
       display: inline-block;
-      border-radius:8px;
+      border-radius: 8px;
       transition: 0.3s ease background-color;
-      &:hover{
+      &:hover {
         background-color: var(--deep-dark);
       }
-    a{
-      display:inline-block;
+    }
+    a {
+      display: inline-block;
       font-family: 'RobotoMono Regular';
-      padding 1rem 2rem;
+      padding: 1rem 2rem;
       font-size: 2rem;
       color: var(--gray-1);
-      outline:none;
+      outline: none;
     }
-    .active{
+    .active {
       color: var(--white);
+    }
+  }
+  .mobile-menu-icon {
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    width: 4rem;
+    cursor: pointer;
+    display: none;
+    outline: none;
+  }
+  .closeNavIcon {
+    display: none;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 0;
+    .hide-item {
+      transform: translateY(calc(-100% - var(--top)));
+      display: none;
+    }
+    .mobile-menu-icon {
+      display: block;
+    }
+    .closeNavIcon {
+      display: none;
+    }
+    .navItems {
+      --top: 1rem;
+      transition: 0.3s ease transform;
+      background-color: var(--deep-dark);
+      padding: 2rem;
+      width: 90%;
+      max-width: 300px;
+      border-radius: 12px;
+      position: absolute;
+      right: 1rem;
+      top: var(--top);
+      .closeNavIcon {
+        display: block;
+        width: 3rem;
+        margin: 0 0 0 auto;
+        cursor: pointer;
+        * {
+          pointer-events: none;
+        }
+      }
+      li {
+        display: block;
+        margin-bottom: 1rem;
+      }
     }
   }
 `;
 
 export default function NavBar() {
+  const [showNav, SetShowNav] = useState(false);
   return (
     <NavBarStyles>
-      <div className="mobile-menu-icon" />
-      <ul>
+      <div
+        className="mobile-menu-icon"
+        onClick={() => SetShowNav(!showNav)}
+        role="button"
+        onKeyDown={() => SetShowNav(!showNav)}
+        tabIndex={0}
+      >
+        <MdMenu />
+      </div>
+      <ul className={!showNav ? 'navItems hide-item' : 'navItems '}>
+        <div
+          className="closeNavIcon"
+          onClick={() => SetShowNav(!showNav)}
+          role="button"
+          onKeyDown={() => SetShowNav(!showNav)}
+          tabIndex={0}
+        >
+          <MdClose />
+        </div>
         <li>
-          <NavLink to="/" exact>
+          <NavLink
+            to="/"
+            exact
+            onClick={() => SetShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/about">About</NavLink>
+          <NavLink
+            to="/about"
+            onClick={() => SetShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
+            About
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/projects">Projects</NavLink>
+          <NavLink
+            to="/projects"
+            onClick={() => SetShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
+            Projects
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink
+            to="/contact"
+            onClick={() => SetShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
+            Contact
+          </NavLink>
         </li>
       </ul>
     </NavBarStyles>
